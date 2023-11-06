5e2abc7d889248c840937c8d25f2d0048511f10f
Lindsof <lindbergsof@gmail.com>
2023-11-06 17:51:41 +0200

    e

--- 05.10CarShareEx.js
+++ 05.10CarShareEx.js
@@ -1,10 +1,9 @@
-function calculateCosts() {
-var kilometers = document.getElementById("kilometers").value;
-var consumption =document.getElementById("consumption").value;
-var price = document.getElementById("price").value;
-var participants = document.getElementById ("participants").value;
+car Shared
 
-var costPerParticipant = (consumption/100*price*kilometers/participants);
 
-document.getElementById("answerId").innerHTML ="Fuel costs per participant is "+ costPerParticipant.toFixed(2) + " euros.";
-}
+
+
+
+
+
+
